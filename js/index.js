7c1e453d9081ad06f61b1a74cef8b370311338a0
Jessica Jin <jessicajin@Jessicas-MacBook-Pro.local>
2017-03-06 17:29:03 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,74 @@
+/*
+ * This is Jessica Jin's adaptation to previous code found on CodePen. 
+ *
+ * The original code for the Star Wars opening crawl from 1977
+ * was developed by Tim Pietrusky (timpietrusky.com) in 2013.
+ * He wrote an article where he explains how this works:
+ * http://timpietrusky.com/star-wars-opening-crawl-from-1977
+ * 
+ * Things used:
+ * - CSS (animation, transform)
+ * - HTML audio (the opening theme)
+ * - SVG (the Star Wars logo from wikimedia.org)
+ *   http://commons.wikimedia.org/wiki/File:Star_Wars_Logo.svg
+ * - JavaScript (to sync the animation/audio)
+ *
+ * Thanks to Craig Buckler for his amazing article 
+ * which helped me to create this remake of the Star Wars opening crawl. 
+ * http://www.sitepoint.com/css3-starwars-scrolling-text/ 
+ *
+ * Sound copyright by The Walt Disney Company.
+ * 
+ */
+
+StarWars = (function() {
+  
+  /* 
+   * Constructor
+   */
+  function StarWars(args) {
+    // Context wrapper
+    this.el = $(args.el);
+    
+    // Audio to play the opening crawl
+    this.audio = this.el.find('audio').get(0);
+    
+    // Start the animation
+    this.start = this.el.find('.start');
+    
+    // The animation wrapper
+    this.animation = this.el.find('.animation');
+    
+    // Remove animation and shows the start screen
+    this.reset();
+
+    // Start the animation on click
+    this.start.bind('click', $.proxy(function() {
+      this.start.hide();
+      this.audio.play();
+      this.el.append(this.animation);
+    }, this));
+    
+    // Reset the animation and shows the start screen
+    $(this.audio).bind('ended', $.proxy(function() {
+      this.audio.currentTime = 0;
+      this.reset();
+    }, this));
+  }
+  
+  /*
+   * Resets the animation and shows the start screen.
+   */
+  StarWars.prototype.reset = function() {
+    this.start.show();
+    this.cloned = this.animation.clone(true);
+    this.animation.remove();
+    this.animation = this.cloned;
+  };
+
+  return StarWars;
+})();
+
+new StarWars({
+  el : '.starwars'
+});
